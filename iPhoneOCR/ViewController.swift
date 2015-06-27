5244a495461a6f1c34af93ef6afc250d31df288f
Allan Jiang <origami789@gmail.com>
2015-06-27 16:20:58 -0700

    install TesseractOCRiOS

---
+++ iPhoneOCR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iPhoneOCR
+//
+//  Created by Tian-Shun Allan Jiang on 6/27/15.
+//  Copyright (c) 2015 Tian-Shun Allan Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
